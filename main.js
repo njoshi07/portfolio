44faecd6ada675deab2567f70bed8ae51ff14290
Nishit <nishitdjoshi@gmail.com>
2020-11-25 20:02:30 -0600

    media query update

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const navSlide = () => {
     const burger = document.querySelector(".burger");
     const navLinks = document.querySelector(".nav-links");
     const nav = document.querySelectorAll(".nav-links li");
-    console.log(nav);
     burger.addEventListener('click',()=> {
         navLinks.classList.toggle("nav-active");
     });
